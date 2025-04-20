8d5fca90f1ae854f8936be7d200718e7f48672af
Marco Barlera <143283524+ZonatedCord@users.noreply.github.com>
2025-04-20 15:31:35 +0200

    Avevo fatto casino, sistemato tutto

--- src/UtenteNonRegistrato.java
+++ src/UtenteNonRegistrato.java
@@ -2,6 +2,19 @@ package src;
 
 public class UtenteNonRegistrato extends Utente{
     public UtenteNonRegistrato(){
-        super();
+        super(" ", " ", "Guest", " "," ", "Utente non registrato", 00,00, 0000);
+    }
+
+    public String getDettagli(Ristorante ristorante){
+        return ristorante.toString();
+    }
+
+    public void getRecensioni(Ristorante ristorante){
+        ristorante.getRecensioni();
+    }
+
+    public ClienteRegistrato Registrare(String Nome, String Cognome, String Username, String Password, String Domicilio, int Giorno, int Mese, int Anno){
+        ClienteRegistrato cl = new ClienteRegistrato();
+        return cl;
     }
 }

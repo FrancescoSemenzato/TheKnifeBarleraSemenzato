1ccaf020a462de36bf1d2edac9937fbae1bf744c
Marco Barlera <143283524+ZonatedCord@users.noreply.github.com>
2025-04-20 15:22:17 +0200

    Revert "Merge branch 'main' of https://github.com/FrancescoSemenzato/TheKnifeBarleraSemenzato into Prova_Barle"
    This reverts commit 14ba6cae52be424c05c6605fdff6acfd7a53e2e0, reversing
    changes made to f7f8d781e92f7bfcd46b15636ba6b8e0ec121445.

--- src/UtenteNonRegistrato.java
+++ src/UtenteNonRegistrato.java
@@ -1,19 +1,7 @@
 package src;
+
 public class UtenteNonRegistrato extends Utente{
     public UtenteNonRegistrato(){
-        super(" ", " ", "Guest", " "," ", "Utente non registrato", 00,00, 0000);
-    }
-
-    public String getDettagli(Ristorante ristorante){
-        return ristorante.toString();
-    }
-
-    public void getRecensioni(Ristorante ristorante){
-        ristorante.getRecensioni();
-    }
-
-    public ClienteRegistrato Registrare(String Nome, String Cognome, String Username, String Password, String Domicilio, int Giorno, int Mese, int Anno){
-        ClienteRegistrato cl = new ClienteRegistrato();
-        return cl;
+        super();
     }
 }
